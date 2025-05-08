28c224d999310af33cc4a366b5301743c6ecec1e
Emipalacios22 <hpalacios0@ucol.mx>
2025-05-08 11:51:47 -0600

    registerAdd

---
+++ js/regScript.js
@@ -0,0 +1,6 @@
+document.getElementById('registroForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('¡Registro exitoso!');
+    window.location.href = 'index.html'; // Redirige tras registro exitoso
+  });
+  
