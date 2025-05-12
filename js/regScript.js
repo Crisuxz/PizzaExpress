5e8bea860c0c6b210d9207956d7c4f9d7201fa00
Crisuxz <cfigueroa21@ucol.mx>
2025-05-12 16:22:04 -0600

    p1

--- js/regScript.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('registroForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('¡Registro exitoso!');
-    window.location.href = 'index.html'; // Redirige tras registro exitoso
-  });
-  
